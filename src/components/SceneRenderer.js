988d5211f3aaa263a8b4a22f684fbce67682427b
Sanjay-adepu <adepusanjay812@gmail.com>
2024-10-31 10:38:15 +0530

    Update SceneRenderer.js

--- src/components/SceneRenderer.js
+++ src/components/SceneRenderer.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Environment } from '@react-three/drei';
 import Model from './Model';
 
 function SceneRenderer({ sceneData }) {
+    useEffect(() => {
+        console.log("Rendering SceneRenderer with data:", sceneData);
+    }, [sceneData]);
+
     return (
-        <Canvas style={{ background: '#cccccc' }}>
+        <Canvas style={{ background: '#cccccc', width: '100vw', height: '100vh' }}>
             <ambientLight intensity={0.5} />
             <pointLight position={[10, 10, 10]} />
             <OrbitControls />
